42debd15cdeea77ad2c6f9f70cc0a30a17bab2a8
Kevin Steinsapir <kevinsteinsapir@gmail.com>
2024-06-18 11:55:37 -0400

    specified certain routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :datexes do
-    resources :bookings
-    resources :reviews
+    resources :bookings, only: %i[new create]
+    resources :reviews, only: %i[new create]
   end
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
