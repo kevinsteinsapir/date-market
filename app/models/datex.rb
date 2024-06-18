9aa9c49064047ec440cb794051a758e32a408b8b
Kevin Steinsapir <kevinsteinsapir@gmail.com>
2024-06-18 11:52:44 -0400

    added resources general routes

--- app/models/datex.rb
+++ app/models/datex.rb
@@ -1,4 +1,5 @@
 class Datex < ApplicationRecord
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, dependant: :destroy
+  has_many :reviews, dependant: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_17_204521) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_18_154626) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_204521) do
     t.index ["user_id"], name: "index_datexes_on_user_id"
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.integer "rating"
+    t.text "comment"
+    t.bigint "datex_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["datex_id"], name: "index_reviews_on_datex_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -53,4 +62,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_17_204521) do
   add_foreign_key "bookings", "datexes"
   add_foreign_key "bookings", "users"
   add_foreign_key "datexes", "users"
+  add_foreign_key "reviews", "datexes"
 end
