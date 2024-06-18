4483da213ac87f560f418850d668c01e2b6f3e26
Andres delaTorre <andresdevlatorre@gmail.com>
2024-06-18 15:03:23 -0500

    controller casi terminado

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,44 @@
 class BookingsController < ApplicationController
-
   def index
-    @reviews = Review.all
+    @bookings = Booking.all
   end
 
   def show
-    @review = Review.find(params[:id])
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @datex = Datex.find(params[:datex_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @datex = Datex.find(params[:datex_id])
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    @booking.datex = @datex
+    if @booking.save
+      redirect_to root_path, notice: 'Date booked!'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    raise
+    @booking = Booking.find(params[:datex_id])
+    
+  end
+
+  def updated
+  end
+
+  def delete
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:fecha)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :datexes do
+    resources :bookings, only: %i[new create edit]
+  end
   devise_for :users
   root to: "pages#home"
-  resources :reviews
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
