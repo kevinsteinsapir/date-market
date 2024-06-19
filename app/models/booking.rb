02ea21fcb0f340afd0bd63699b51224690205b1a
Pablo Davila <pablo0davila4@gmail.com>
2024-06-19 11:31:01 -0400

    removed date validation for booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,13 +2,4 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :datex
   validates :user_id, :datex_id, :fecha, presence: true
-  validate :fecha_format
-
-  private
-
-  def fecha_format
-    Date.strptime(fecha, "%d-%m-%Y")
-  rescue ArgumentError
-    errors.add(:fecha, "must be a valid date in the format dd-mm-yyyy")
-  end
 end
