3fc75d7afa563d7151b29bd990b40dade88e47a3
Kevin Steinsapir <kevinsteinsapir@gmail.com>
2024-06-19 00:03:55 -0400

    Made some work with address and geocoding

--- app/models/datex.rb
+++ app/models/datex.rb
@@ -3,4 +3,7 @@ class Datex < ApplicationRecord
   CATEGORIES = %w[Category A Category B Category C Category D Category E Category F].freeze
   has_many :bookings #dependant: :destroy
   has_many :reviews #dependant: :destroy
+
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
